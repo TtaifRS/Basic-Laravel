529f9ee16965730040ae14ac813d5bddf4857e77
Taif Rahaman <taif.rahaman73@gmail.com>
2021-06-20 16:50:15 +0600

    basic paginate with serial no

--- resources/views/admin/category/index.blade.php
+++ resources/views/admin/category/index.blade.php
@@ -11,62 +11,65 @@
                 <div class="col-md-8">
                     <div class="card">
                         @if (session('success'))
-                        <div class="alert alert-success alert-dismissible fade show" role="alert">
-                            <strong>{{session('success')}}</strong> 
-                            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                          </div>
+                            <div class="alert alert-success alert-dismissible fade show" role="alert">
+                                <strong>{{ session('success') }}</strong>
+                                <button type="button" class="btn-close" data-bs-dismiss="alert"
+                                    aria-label="Close"></button>
+                            </div>
 
                         @endif
-                       
+
                         <div class="card-header">
                             All Category
                         </div>
-                            <table class="table table-dark table-striped">
-                                <thead>
-                                  <tr>
+                        <table class="table table-dark table-striped">
+                            <thead>
+                                <tr>
                                     <th scope="col">SL No</th>
                                     <th scope="col">Category Name</th>
                                     <th scope="col">User</th>
                                     <th scope="col">Created At</th>
-                                  </tr>
-                                </thead>
-                                <tbody>
-                                    @php($i = 1)
-                                    @foreach ($categories as $category )
-                                        <tr>
-                                            <th scope="row">{{$i++}}</th>
-                                            <td>{{$category->category_name}}</td>
-                                            <td>{{$category->user_id}}</td>
-                                            <td>{{$category->created_at->diffForHumans()}}</td>
-                                            {{-- Query builder --}}
-                                            {{-- <td>{{Carbon\Carbon::parse($user->created_at)->diffForHumans()}}</td> --}}
-                                        </tr>
-                                    @endforeach
-                                        
-                                    
-                                </tbody>
-                              </table>
+                                </tr>
+                            </thead>
+                            <tbody>
+
+                                @foreach ($categories as $category)
+                                    <tr>
+                                        <th scope="row">{{ $categories->firstItem() + $loop->index }}</th>
+                                        <td>{{ $category->category_name }}</td>
+                                        <td>{{ $category->user_id }}</td>
+                                        <td>{{ $category->created_at->diffForHumans() }}</td>
+                                        {{-- Query builder --}}
+                                        {{-- <td>{{Carbon\Carbon::parse($user->created_at)->diffForHumans()}}</td> --}}
+                                    </tr>
+                                @endforeach
+
+
+                            </tbody>
+                        </table>
+                        {{ $categories->links() }}
                     </div>
                 </div>
                 <div class="col-md-4">
                     <div class="card">
                         <div class="card-header">Add Category</div>
                         <div class="card-body">
-                            <form class="px-3" action="{{route('store.category')}}" method="POST">
+                            <form class="px-3" action="{{ route('store.category') }}" method="POST">
                                 @csrf
                                 <div class="form-group">
                                     <label for="exampleInputEmail1" class="form-label">Category Name</label>
-                                    <input type="text" name="category_name" class="form-control mt-1" id="exampleInputEmail1" aria-describedby="emailHelp">
+                                    <input type="text" name="category_name" class="form-control mt-1"
+                                        id="exampleInputEmail1" aria-describedby="emailHelp">
                                     @error('category_name')
-                                        <span class="text-danger">{{$message}}</span>
+                                        <span class="text-danger">{{ $message }}</span>
                                     @enderror
-                                </div>  
+                                </div>
                                 <button type="submit" class="btn btn-dark my-2">Add Category</button>
                             </form>
                         </div>
                     </div>
                 </div>
-               
+
             </div>
         </div>
     </div>
