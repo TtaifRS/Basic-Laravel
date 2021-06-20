88cfffa1ea1c5030ae831067295cc146af05ea57
Taif Rahaman <taif.rahaman73@gmail.com>
2021-06-20 12:33:07 +0600

    Eloquent ORM inser data with success message

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Redirect;
 
 class CategoryController extends Controller
 {
@@ -21,5 +25,21 @@ class CategoryController extends Controller
                 'category_name.required' => 'Please Input Catergory Name'
             ]
         );
+
+        //eloquent ORM insert first way
+
+        // Category::insert([
+        //     'category_name' => $request->category_name,
+        //     'user_id' => Auth::user()->id,
+        //     'created_at' => Carbon::now()
+        // ]);
+
+        //eloquent ORM insert main way
+        $categoy = new Category;
+        $categoy->category_name = $request->category_name;
+        $categoy->user_id = Auth::user()->id;
+        $categoy->save();
+
+        return Redirect()->back()->with('success', 'Category Inserted Successfully');
     }
 }
--- resources/views/admin/category/index.blade.php
+++ resources/views/admin/category/index.blade.php
@@ -8,8 +8,20 @@
     <div class="py-12">
         <div class="container">
             <div class="row">
+
+
+
+
                 <div class="col-md-8">
                     <div class="card">
+                        @if (session('success'))
+                        <div class="alert alert-warning alert-dismissible fade show" role="alert">
+                            <strong>{{session('success')}}</strong> 
+                            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                          </div>
+
+                        @endif
+                       
                         <div class="card-header">
                             All Category
                         </div>
